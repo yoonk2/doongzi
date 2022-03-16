cb4bf31bdee65c90c0a3c78b34e7a7a763dea1a1
Yoon <yoonk0212@ewhain.net>
2022-03-16 20:45:33 +0900

    allowed hosts

--- config/config/settings.py
+++ config/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)+4f2a!#f171uod#(ijev%@qij7(-(@+76x1y@h+2dty%8le#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
