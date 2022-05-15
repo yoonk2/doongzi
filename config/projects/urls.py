6584a70f889cc16e55fbc5aa270e41ba34b4e0a8
Yoon <yoonk0212@ewhain.net>
2022-05-15 16:29:39 +0900

    [FEAT] 스승의날 프로젝트 config

--- config/projects/urls.py
+++ config/projects/urls.py
@@ -8,4 +8,7 @@ app_name = 'projects'
 urlpatterns = [
     path('', index, name='index'),
     path('yoon-kwon/', yoon_kwon, name='yoon-kwon'),
+    path('thank-you/', thank_you, name='thank-you'),
+    path('thank-you/1', thank_you_1, name='thank-you-1'),
+    path('thank-you/2', thank_you_2, name='thank-you-2'),
 ]
--- config/projects/views.py
+++ config/projects/views.py
@@ -7,3 +7,28 @@ def index(request):
 
 def yoon_kwon(request):
     return render(request,'yoon-kwon.html')
+
+def thank_you(request):
+    return render(request,'thank-you.html')
+
+def thank_you_1(request):
+    ctx = {'name' : '최장순'}
+    return render(request,template_name='thank-you.html', context=ctx)
+def thank_you_2(request):
+    ctx = {'name' : '김나연'}
+    return render(request,template_name='thank-you.html', context=ctx)
+def thank_you_3(request):
+    ctx = {'name' : '최장순'}
+    return render(request,template_name='thank-you.html', context=ctx)
+def thank_you_4(request):
+    ctx = {'name' : '최장순'}
+    return render(request,template_name='thank-you.html', context=ctx)
+def thank_you_5(request):
+    ctx = {'name' : '최장순'}
+    return render(request,template_name='thank-you.html', context=ctx)
+def thank_you_6(request):
+    ctx = {'name' : '최장순'}
+    return render(request,template_name='thank-you.html', context=ctx)
+def thank_you_7(request):
+    ctx = {'name' : '최장순'}
+    return render(request,template_name='thank-you.html', context=ctx)
