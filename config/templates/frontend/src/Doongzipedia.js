b9b54050a2a98edb1a056065d032afd5a4e77a7f
Yoon <yoonk0212@ewhain.net>
2022-08-03 23:21:49 +0900

    [feat] auto scrolling

--- config/templates/frontend/src/Doongzipedia.js
+++ config/templates/frontend/src/Doongzipedia.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import "./Doongzipedia.css"
 import Word from "./components/Word.js"
 
@@ -9,6 +9,7 @@ function DoonzgiPedia() {
 		const response = await fetch("http://127.0.0.1:8000/api/words/")
 		const data = await response.json()
 		setWordList(data)
+		console.log("success!")
 	}
 	const [wordList, setWordList] = useState([])
 
@@ -16,10 +17,6 @@ function DoonzgiPedia() {
 		array.sort(() => Math.random() - 0.5)
 		return array
 	}
-
-	// setTimeout(() => {
-	// 	wordColumns[0].scrollTop += 10
-	// }, 100)
 
 	const animateDoong = (time) => {
 		const interval = setInterval(() => {
@@ -49,30 +46,58 @@ function DoonzgiPedia() {
 		}
 	}
 
-	function scrollDown(amount, element) {
-		let currentScrollTop = element.scrollTop
-		if (currentScrollTop >= element.scrollHeight - window.innerHeight) {
-			element.scrollTo({
-				top: 0,
-				behavior: "smooth",
-			})
+	let scrollDownList = [true, true, true]
+	const scroll = (element, scrollDown) => {
+		if (scrollDown) {
+			console.log("down", scrollDown)
+			element.scrollBy(0, 1)
 		} else {
-			element.scrollTo({
-				top: (currentScrollTop += amount),
-				behavior: "smooth",
+			element.scrollBy(0, -1)
+			console.log("up", scrollDown)
+		}
+
+		if (element.scrollTop > element.scrollHeight - window.innerHeight * 0.95) {
+			scrollDown = false
+		}
+		if (element.scrollTop === 0) {
+			scrollDown = true
+			console.log("scrollDown", scrollDown)
+		}
+		return scrollDown
+	}
+
+	let pausedList = [false, false, false]
+
+	const columns = useRef([])
+
+	const autoScroll = () => {
+		setInterval(() => {
+			if (!pausedList[0]) {
+				scrollDownList[0] = scroll(columns.current[0], scrollDownList[0])
+			}
+			if (columns.current.length > 2) {
+				if (!pausedList[1]) {
+					scrollDownList[1] = scroll(columns.current[1], scrollDownList[1])
+				}
+				if (!pausedList[2]) {
+					scrollDownList[2] = scroll(columns.current[2], scrollDownList[2])
+					console.log("scrollDownList", scrollDownList)
+				}
+			}
+		}, 20)
+	}
+
+	const setPaused = () => {
+		for (let i = 0; i < pausedList.length; i++) {
+			columns.current[i].addEventListener("mouseenter", (e) => {
+				pausedList[i] = true
+			})
+			columns.current[i].addEventListener("mouseleave", (e) => {
+				pausedList[i] = false
 			})
 		}
 	}
-	// setInterval(() => {
-	// 	scrollDown(100)
-	// }, 1000)
 
-	const wordColumns = document.querySelectorAll(".word-col")
-	wordColumns.forEach((wordColumn) => {
-		wordColumn.addEventListener("mouseenter", (e) => {
-			scrollDown(100, wordColumn)
-		})
-	})
 	useEffect(() => {
 		getData()
 		animateDoong(123)
@@ -80,12 +105,27 @@ function DoonzgiPedia() {
 		animateDoong(64)
 		animateDoong(70)
 		animateDoong(33)
+		autoScroll()
+		setTimeout(() => {
+			clearInterval(animateDoong(123))
+			clearInterval(animateDoong(50))
+			clearInterval(animateDoong(64))
+			clearInterval(animateDoong(70))
+			clearInterval(animateDoong(33))
+		}, 5000)
+		autoScroll()
+		setPaused()
 	}, [])
 	return (
 		<>
 			<main>
 				<div className="word-wrapper">
-					<section className="word-col">
+					<section
+						className="word-col"
+						ref={(element) => {
+							columns.current[0] = element
+						}}
+					>
 						{shuffle(wordList).map((word) => (
 							<Word word={word} />
 						))}
@@ -94,14 +134,24 @@ function DoonzgiPedia() {
 				{window.innerWidth > 500 ? (
 					<>
 						<div className="word-wrapper">
-							<section className="word-col">
+							<section
+								className="word-col"
+								ref={(element) => {
+									columns.current[1] = element
+								}}
+							>
 								{shuffle(wordList).map((word) => (
 									<Word word={word} />
 								))}
 							</section>
 						</div>
 						<div className="word-wrapper">
-							<section className="word-col">
+							<section
+								className="word-col"
+								ref={(element) => {
+									columns.current[2] = element
+								}}
+							>
 								{shuffle(wordList).map((word) => (
 									<Word word={word} />
 								))}
