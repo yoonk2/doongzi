d696cb9d31f70737afd4206fdc46e03be635c03e
Yoon Kwon <yoonk0212@gmail.com>
2023-04-16 22:58:17 +0900

    개발자 to 개발자 꿈나무fix

--- config/static/js/yoon-kwon.js
+++ config/static/js/yoon-kwon.js
@@ -21,7 +21,7 @@ designerTitle.addEventListener("click", () => {
 developerTitle.addEventListener("click", () => {
 	if (developerDetails[0].classList.contains("hidden")) {
 		developerDetails.forEach((item) => item.classList.toggle("hidden"))
-		developerTitle.innerHTML = `<a href="https://github.com/yoonk2">개발자</a>다.`
+		developerTitle.innerHTML = `<a href="https://github.com/yoonk2">개발자 꿈나무</a>다.`
 		developerTitleAnchor.classList.remove("fade")
 		studentTitleAnchor.classList.add("fade")
 	}
